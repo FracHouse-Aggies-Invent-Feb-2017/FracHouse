2f0945eb7809c9ff2bcf3fa9db569f405dd69438
roflmaoqwertyqaz <tkdduo@gmail.com>
2017-02-04 10:55:58 -0600

    Added think method

--- core/src/aggiesinvent/frachouse/FracHouseMain.java
+++ core/src/aggiesinvent/frachouse/FracHouseMain.java
@@ -18,13 +18,18 @@ public class FracHouseMain extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		think();
 		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
+	public void think() {
+
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
